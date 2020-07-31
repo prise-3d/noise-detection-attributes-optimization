6f3a6f515337db621579125ad6704be032e5c662
Jérôme BUISINE <contact@jeromebuisine.fr>
2020-07-31 21:19:02 +0200

    model parameters reduced

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.feature_selection import RFECV
 import sklearn.svm as svm
-from sklearn.metrics.scorer import accuracy_scorer
+from sklearn.metrics import accuracy_scorer
 from thundersvm import SVC
 
 # variables and parameters
@@ -21,12 +21,12 @@ def my_accuracy_scorer(*args):
 
 def _get_best_model(X_train, y_train):
 
-    Cs = [0.001, 0.01, 0.1, 1, 2, 5, 10, 100, 1000]
-    gammas = [0.001, 0.01, 0.1, 1, 2, 5, 10, 100]
+    Cs = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
+    gammas = [0.001, 0.01, 0.1, 5, 10, 100]
     param_grid = {'kernel':['rbf'], 'C': Cs, 'gamma' : gammas}
 
     svc = svm.SVC(probability=True, class_weight='balanced')
-    clf = GridSearchCV(svc, param_grid, cv=10, verbose=1, scoring=my_accuracy_scorer, n_jobs=-1)
+    clf = GridSearchCV(svc, param_grid, cv=5, verbose=1, scoring=my_accuracy_scorer, n_jobs=-1)
 
     clf.fit(X_train, y_train)
 
