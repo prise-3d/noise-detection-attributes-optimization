2a0cb4ed2dda6b801a5dbf746860554369182c5c
Jérôme BUISINE <contact@jeromebuisine.fr>
2021-03-31 16:48:19 +0200

    add of 10 times surrogate runs

--- run_surrogate_rendering.sh
+++ run_surrogate_rendering.sh
@@ -13,16 +13,17 @@ TRAIN_EVERY=50
 #output="rendering-attributes-ILS_${ILS}-POP_${POP}-LS_${LS}-SS_${SS}-SO_${ORDER}-SE_${TRAIN_EVERY}"
 DATASET="rnn/data/datasets/features-selection-rendering-scaled/features-selection-rendering-scaled"
 
-for POP in {20,60,100};
-do
-    for ORDER in {1,2,3};
+for run in {1,2,3,4,5,6,7,8,9,10};
+    for POP in {20,60,100};
     do
-        for LS in {1000,5000,10000};
+        for ORDER in {1,2,3};
         do
-            output="rendering-attributes-POP_${POP}-LS_${LS}-SS_${SS}-SO_${ORDER}-SE_${TRAIN_EVERY}"
-            echo "Run optim attributes using: ${output}"
-            python find_best_attributes_surrogate.py --data ${DATASET} --start_surrogate ${SS} --length 32 --ils ${ILS} --ls ${LS} --pop ${POP} --order ${ORDER} --train_every ${TRAIN_EVERY}  --output ${output}
+            for LS in {1000,5000,10000};
+            do
+                output="rendering-attributes-POP_${POP}-LS_${LS}-SS_${SS}-SO_${ORDER}-SE_${TRAIN_EVERY}-RUN_${run}"
+                echo "Run optim attributes using: ${output}"
+                python find_best_attributes_surrogate.py --data ${DATASET} --start_surrogate ${SS} --length 32 --ils ${ILS} --ls ${LS} --pop ${POP} --order ${ORDER} --train_every ${TRAIN_EVERY}  --output ${output}
+            done
         done
     done
 done
-
